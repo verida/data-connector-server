7880d2cdf144e5e84dece8fe9deea83af09ec122
Chris <chris@verida.io>
2024-07-22 11:53:20 +0930

    Fix base sync handler saving issue

--- src/providers/BaseSyncHandler.ts
+++ src/providers/BaseSyncHandler.ts
@@ -120,11 +120,12 @@ export default class BaseSyncHandler extends EventEmitter {
                 continue
             }
 
-            const schemaDatastore = await this.provider.getDatastore(item.schema)
+            const schemaDatastore = await this.provider.getDatastore(this.getSchemaUri())
 
             try {
                 const success = await schemaDatastore.save(item, {})
                 if (!success) {
+                    // @ts-ignore
                     const message = `Unable to save item: ${Utils.datastoreErorrsToString(schemaDatastore.errors)} (${JSON.stringify(item, null, 2)})`
 
                     this.emit('error', {
