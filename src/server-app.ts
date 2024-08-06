0544b467c313da78ebbb537a69ab87eb2ce39afc
Chris <chris@verida.io>
2024-08-06 15:13:39 +0930

    Add dashboard to express

--- src/server-app.ts
+++ src/server-app.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import { UniqueRequest } from './interfaces'
+import express, { Response, NextFunction } from 'express'
 const cors = require('cors')
 import bodyParser from 'body-parser'
 import router from './routes'
 // @todo: See not in express-session about not using memory session
 const session = require('express-session')
+import { v4 as uuidv4 } from 'uuid';
 
 const log4js = require("log4js")
 const logger = log4js.getLogger()
@@ -12,10 +14,18 @@ logger.level = CONFIG.logLevel
 
 const path = require('path')
 
+function requestIdMiddleware(req: UniqueRequest, res: Response, next: NextFunction): void {
+  req.requestId = uuidv4();
+  next();
+}
+
+
 // Set up the express app
 const app = express();
 
+app.use(requestIdMiddleware)
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/dashboard', express.static(path.join(__dirname, 'dashboard')))
 app.use(session({
   secret: 'c20n498n720489t729amx9 8es',
   resave: false,
