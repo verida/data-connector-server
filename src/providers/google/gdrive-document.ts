eb25f8a88290e95881b900eb48a353282b2603f8
chime3 <chidev0104@gmail.com>
2024-08-20 22:54:14 -0700

    fix: sync handler status

--- src/providers/google/gdrive-document.ts
+++ src/providers/google/gdrive-document.ts
@@ -113,11 +113,11 @@ export default class GoogleDriveDocument extends BaseSyncHandler {
     }
 
     protected stopSync(syncPosition: SyncHandlerPosition): SyncHandlerPosition {
-        if (syncPosition.status == SyncHandlerStatus.STOPPED) {
+        if (syncPosition.status == SyncHandlerStatus.ENABLED) {
             return syncPosition;
         }
 
-        syncPosition.status = SyncHandlerStatus.STOPPED;
+        syncPosition.status = SyncHandlerStatus.ENABLED;
         syncPosition.thisRef = undefined;
         syncPosition.breakId = syncPosition.futureBreakId;
         syncPosition.futureBreakId = undefined;
