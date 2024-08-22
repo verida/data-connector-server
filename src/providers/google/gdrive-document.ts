a079e2456aab250b640efedbb9cd11ed6c6eac59
chime3 <chidev0104@gmail.com>
2024-08-22 06:18:04 -0700

    refactor: optimize sync and fields

--- src/providers/google/gdrive-document.ts
+++ src/providers/google/gdrive-document.ts
@@ -1,18 +1,20 @@
 import BaseSyncHandler from "../BaseSyncHandler";
 import CONFIG from "../../config";
-import { SyncProviderLogEvent, SyncProviderLogLevel } from '../../interfaces'
-import {
-    SyncResponse,
-    SyncHandlerPosition,
-    SyncHandlerStatus,
-} from "../../interfaces";
+import { SyncProviderLogEvent, SyncProviderLogLevel, SyncHandlerPosition, SyncItemsBreak, SyncResponse, SyncHandlerStatus, SyncItemsResult, HandlerOption, ConnectionOptionType } from '../../interfaces';
 import { SchemaDocument } from "../../schemas";
 import { google, drive_v3 } from "googleapis";
 import { OAuth2Client } from 'google-auth-library';
 import { GaxiosResponse } from "gaxios";
 import { GoogleDriveHelpers } from "./helpers";
+import { ItemsRangeTracker } from "../../helpers/itemsRangeTracker";
 
 const _ = require("lodash");
+
+const MAX_BATCH_SIZE = 50;
+
+export interface SyncDocumentItemsResult extends SyncItemsResult {
+    items: SchemaDocument[];
+}
 
 export default class GoogleDriveDocument extends BaseSyncHandler {
 
@@ -27,7 +29,7 @@ export default class GoogleDriveDocument extends BaseSyncHandler {
     public getProviderApplicationUrl(): string {
         return "https://drive.google.com";
     }
-    
+
     public getGoogleAuth(): OAuth2Client {
         const TOKEN = {
             access_token: this.connection.accessToken,
@@ -50,147 +52,180 @@ export default class GoogleDriveDocument extends BaseSyncHandler {
     }
 
     public getGoogleDrive(): drive_v3.Drive {
-        
         const auth = this.getGoogleAuth();
+        return google.drive({ version: "v3", auth });
+    }
 
-        const drive = google.drive({ version: "v3", auth: auth });
-        return drive;
+    public getOptions(): HandlerOption[] {
+        return [{
+            name: 'backdate',
+            label: 'Backdate history',
+            type: ConnectionOptionType.ENUM,
+            enumOptions: ['1 month', '3 months', '6 months', '12 months'],
+            defaultValue: '3 months'
+        }];
     }
 
     public async _sync(
         api: any,
         syncPosition: SyncHandlerPosition
     ): Promise<SyncResponse> {
-        const drive = this.getGoogleDrive();
-        const auth = this.getGoogleAuth();
+        if (this.config.batchSize > MAX_BATCH_SIZE) {
+            throw new Error(`Batch size (${this.config.batchSize}) is larger than permitted (${MAX_BATCH_SIZE})`);
+        }
 
-        const query: drive_v3.Params$Resource$Files$List = {
+        const drive = this.getGoogleDrive();
+        const rangeTracker = new ItemsRangeTracker(syncPosition.thisRef);
+
+        let items: SchemaDocument[] = [];
+        let currentRange = rangeTracker.nextRange();
+        let query: drive_v3.Params$Resource$Files$List = {
             pageSize: this.config.batchSize,
             fields: 'nextPageToken, files(id, name, mimeType, modifiedTime, webViewLink, thumbnailLink)',
             q: "mimeType='application/vnd.google-apps.document' or mimeType='application/vnd.google-apps.spreadsheet' or mimeType='application/vnd.google-apps.presentation' or mimeType='application/pdf' or mimeType='application/vnd.openxmlformats-officedocument.wordprocessingml.document'"
         };
 
-        if (syncPosition.thisRef) {
-            query.pageToken = syncPosition.thisRef;
+        if (currentRange.startId) {
+            query.pageToken = currentRange.startId;
         }
 
-        const serverResponse = await drive.files.list(query);
-
-        if (
-            !_.has(serverResponse, "data.files") ||
-            !serverResponse.data.files.length
-        ) {
-            // No results found, so stop sync
-            syncPosition.syncMessage = "Stopping. No results found.";
-            syncPosition = this.stopSync(syncPosition);
-
-            return {
-                position: syncPosition,
-                results: [],
-            };
-        }
-
-        const results = await this.buildResults(
+        const latestResponse = await drive.files.list(query);
+        const latestResult = await this.buildResults(
             drive,
-            auth,
-            serverResponse,
-            syncPosition.breakId,
+            latestResponse,
+            currentRange.endId,
             _.has(this.config, "metadata.breakTimestamp")
                 ? this.config.metadata.breakTimestamp
                 : undefined
         );
 
-        syncPosition = this.setNextPosition(syncPosition, serverResponse);
+        items = latestResult.items;
+        let nextPageToken = _.get(latestResponse, "data.nextPageToken");
 
-        if (results.length != this.config.batchSize) {
-            // Not a full page of results, so stop sync
-            syncPosition.syncMessage = `Processed ${results.length} items. Stopping. No more results.`;
-            syncPosition = this.stopSync(syncPosition);
+        if (items.length) {
+            rangeTracker.completedRange({
+                startId: items[0].sourceId,
+                endId: nextPageToken
+            }, latestResult.breakHit === SyncItemsBreak.ID);
+        } else {
+            rangeTracker.completedRange({
+                startId: undefined,
+                endId: undefined
+            }, false);
         }
 
+        if (items.length != this.config.batchSize) {
+            currentRange = rangeTracker.nextRange();
+            query = {
+                ...query,
+                pageSize: this.config.batchSize - items.length,
+            };
+
+            if (currentRange.startId) {
+                query.pageToken = currentRange.startId;
+            }
+
+            const backfillResponse = await drive.files.list(query);
+            const backfillResult = await this.buildResults(
+                drive,
+                backfillResponse,
+                currentRange.endId,
+                _.has(this.config, "metadata.breakTimestamp")
+                    ? this.config.metadata.breakTimestamp
+                    : undefined
+            );
+
+            items = items.concat(backfillResult.items);
+            nextPageToken = _.get(backfillResponse, "data.nextPageToken");
+
+            if (backfillResult.items.length) {
+                rangeTracker.completedRange({
+                    startId: backfillResult.items[0].sourceId,
+                    endId: nextPageToken
+                }, backfillResult.breakHit === SyncItemsBreak.ID);
+            } else {
+                rangeTracker.completedRange({
+                    startId: undefined,
+                    endId: undefined
+                }, backfillResult.breakHit === SyncItemsBreak.ID);
+            }
+        }
+
+        if (!items.length) {
+            syncPosition.syncMessage = `Stopping. No results found.`;
+            syncPosition.status = SyncHandlerStatus.ENABLED;
+        } else {
+            syncPosition.syncMessage = items.length != this.config.batchSize && !nextPageToken
+                ? `Processed ${items.length} items. Stopping. No more results.`
+                : `Batch complete (${this.config.batchSize}). More results pending.`;
+        }
+
+        syncPosition.thisRef = rangeTracker.export();
+
         return {
-            results,
+            results: items,
             position: syncPosition,
         };
     }
 
-    protected stopSync(syncPosition: SyncHandlerPosition): SyncHandlerPosition {
-        if (syncPosition.status == SyncHandlerStatus.ENABLED) {
-            return syncPosition;
-        }
-
-        syncPosition.status = SyncHandlerStatus.ENABLED;
-        syncPosition.thisRef = undefined;
-        syncPosition.breakId = syncPosition.futureBreakId;
-        syncPosition.futureBreakId = undefined;
-
-        return syncPosition;
-    }
-
-    protected setNextPosition(
-        syncPosition: SyncHandlerPosition,
-        serverResponse: GaxiosResponse<drive_v3.Schema$FileList>
-    ): SyncHandlerPosition {
-        if (!syncPosition.futureBreakId && serverResponse.data.files.length) {
-            syncPosition.futureBreakId = serverResponse.data.files[0].id;
-        }
-
-        if (_.has(serverResponse, "data.nextPageToken")) {
-            // Have more results, so set the next page ready for the next request
-            syncPosition.syncMessage = `Batch complete (${this.config.batchSize}). More results pending.`;
-            syncPosition.thisRef = serverResponse.data.nextPageToken;
-        } else {
-            syncPosition.syncMessage = "Stopping. No more results.";
-            syncPosition = this.stopSync(syncPosition);
-        }
-
-        return syncPosition;
-    }
-
     protected async buildResults(
         drive: drive_v3.Drive,
-        auth: OAuth2Client,
         serverResponse: GaxiosResponse<drive_v3.Schema$FileList>,
         breakId: string,
         breakTimestamp?: string
-    ): Promise<SchemaDocument[]> {
+    ): Promise<SyncDocumentItemsResult> {
         const results: SchemaDocument[] = [];
+        let breakHit: SyncItemsBreak;
+
         for (const file of serverResponse.data.files) {
             const fileId = file.id;
 
-            if (fileId == breakId) {
-                const logEvent: SyncProviderLogEvent = {
-                    level: SyncProviderLogLevel.DEBUG,
-                    message: `Break ID hit (${breakId})`
-                }
-                this.emit('log', logEvent)
+            if (fileId === breakId) {
+                this.emit('log', { level: SyncProviderLogLevel.DEBUG, message: `Break ID hit (${breakId})` });
+                breakHit = SyncItemsBreak.ID;
                 break;
             }
 
-            const modifiedTime = file.modifiedTime || "Unkown";
-            
+            const createdTime = file.createdTime || new Date().toISOString();
+            const modifiedTime = file.modifiedTime || new Date().toISOString();
+
             if (breakTimestamp && modifiedTime < breakTimestamp) {
-                const logEvent: SyncProviderLogEvent = {
-                    level: SyncProviderLogLevel.DEBUG,
-                    message: `Break timestamp hit (${breakTimestamp})`
-                }
-                this.emit('log', logEvent)
+                this.emit('log', { level: SyncProviderLogLevel.DEBUG, message: `Break timestamp hit (${breakTimestamp})` });
+                breakHit = SyncItemsBreak.TIMESTAMP;
                 break;
             }
 
             const title = file.name || "No title";
-            const link = file.webViewLink || "No link";
-            const mimeType = file.mimeType || "Unknown";
+            const link = file.webViewLink;
+            if (!link) {
+                const logEvent: SyncProviderLogEvent = {
+                    level: SyncProviderLogLevel.DEBUG,
+                    message: `No link available for file ${fileId}. Ignoring this file.`
+                }
+                this.emit('log', logEvent);
+                continue;
+            }
+
+            const mimeType = file.mimeType;
+            if (!mimeType) {
+                const logEvent: SyncProviderLogEvent = {
+                    level: SyncProviderLogLevel.DEBUG,
+                    message: `No mimeType available for file ${fileId}. Ignoring this file.`
+                }
+                this.emit('log', logEvent);
+                continue;
+            }
+
             const type = GoogleDriveHelpers.getDocumentTypeFromMimeType(mimeType);
-            const thumbnail = file.thumbnailLink || "No thumbnail";
-            const size = await GoogleDriveHelpers.getFileSize(drive, file.id)
-            const textContent = await GoogleDriveHelpers.extractIndexableText(drive, file.id, mimeType, auth);
-            
+            const thumbnail = file.thumbnailLink || undefined;
+            const size = await GoogleDriveHelpers.getFileSize(drive, file.id);
+            const textContent = await GoogleDriveHelpers.extractIndexableText(drive, file.id, mimeType, this.getGoogleAuth());
+
             results.push({
                 _id: this.buildItemId(fileId),
                 name: title,
-                type: type,
-                size: size,
+                type,
+                size,
                 uri: link,
                 icon: thumbnail,
                 contentText: textContent,
@@ -198,10 +233,11 @@ export default class GoogleDriveDocument extends BaseSyncHandler {
                 sourceData: file,
                 sourceAccountId: this.provider.getProviderId(),
                 sourceApplication: this.getProviderApplicationUrl(),
-                insertedAt: modifiedTime,
+                insertedAt: createdTime,
+                modifiedAt: modifiedTime,
             });
         }
 
-        return results;
+        return { items: results, breakHit };
     }
 }
