e4bc5202d0a025fb34ce4f8f0ae1b41a3eb9dba0
Chris <chris@verida.io>
2024-09-17 10:08:32 +0930

    Add back deprecated callback endpoint to avoid breaking existing providers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import restRoutes from './api/rest/routes'
 import providerRoutes from './providers/routes'
+import providerController from './providers/controller'
 
 const router = express.Router()
 router.use('/api/rest', restRoutes)
 router.use('/providers', providerRoutes)
+
+// @deprecated callback path, but added for backwards compatibility
+router.get('/callback/:provider', providerController.callback)
 
 export default router
