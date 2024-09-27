762d5e316114b779a14f564dfa7122cf2ab20e0d
Chris <chris@verida.io>
2024-09-27 15:43:36 +0930

    Support ignoring disabled providers

--- src/api/rest/v1/providers/controller.ts
+++ src/api/rest/v1/providers/controller.ts
@@ -70,6 +70,12 @@ export default class Controller {
         const results: ProviderResult[] = []
         for (let p in providers) {
             const providerName = providers[p]
+            const providerConfig = CONFIG.providers[providerName]
+            if (typeof providerConfig['enabled'] === 'boolean' && !providerConfig['enabled']) {
+                // Ignore disabled providers
+                continue
+            }
+
 
             try {
                 const provider = Providers(providerName)
