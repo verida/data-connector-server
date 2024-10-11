f23aaf264b3f192e7630be2403f3144ab176e6a5
chime3 <chidev0104@gmail.com>
2024-10-11 02:25:39 -0700

    fix: modified calendar event handler config

--- src/providers/google/interfaces.ts
+++ src/providers/google/interfaces.ts
@@ -61,6 +61,6 @@ export interface CalendarAttachment {
 }
 
 export interface GoogleCalendarHandlerConfig extends BaseHandlerConfig {
-  calendarBatchSize?: number;  // Max number of calendar per sync
-  eventBatchSize?: number; // Max number of event to process in a calendar
+  calendarBatchSize: number;  // Max number of calendar per sync
+  eventBatchSize: number; // Max number of event to process in a calendar
 }
