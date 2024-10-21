00f50c95195ea5efe3be92c5a9c54e11e56ff44a
aurelticot <aurelticot.dev@gmail.com>
2024-10-21 11:57:24 +1100

    Support whitelisting DIDs for alpha access (#130)
    * feat: add Notion dependency and config
    
    * feat: add custom errors
    
    * feat: implement user access endpoint
    
    * fix: fix import of server config
    
    * feat: add access check to all relevant API endpoints
    
    * feat: add error message in web UI when missing
    
    * feat: update error message on login page
    
    * feat: extract Notion record for user access
    
    * feat: register missing did when checking access
    
    * feat: move access check only for API
    
    * fix: fix login loop when stored key is invalid
    
    * feat: check access on admin endpoints
    
    * Move `memory` and `status` into `/info` endpoints that don't require authorization.

--- src/api/rest/v1/routes.ts
+++ src/api/rest/v1/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
+import { routerV1 as accessRouterV1 } from './access/routes'
 import ProviderRoutes from './providers/routes'
 import ConnectionRoutes from './connections/routes'
 import DbRoutes from './db/routes'
 import DsRoutes from './ds/routes'
 import AdminRoutes from './admin/routes'
+import InfoRoutes from './info/routes'
 import LLMRoutes from './llm/routes'
 import TelegramRoutes from './telegram/routes'
 import SearchRoutes from "./search/routes"
@@ -12,11 +14,13 @@ import AccountRoutes from './account/routes'
 
 const router = express.Router()
 
+router.use('/access', accessRouterV1)
 router.use('/providers', ProviderRoutes)
 router.use('/connections', ConnectionRoutes)
 router.use('/db', DbRoutes)
 router.use('/ds', DsRoutes)
 router.use('/admin', AdminRoutes)
+router.use('/info', InfoRoutes)
 router.use('/llm', LLMRoutes)
 router.use('/search', SearchRoutes)
 router.use('/account', AccountRoutes)
