dccb712b011104eab13755230dadcf6605a8136e
tahpot <chris@verida.network>
2024-11-25 11:27:13 +1030

    Merge develop for 0.2.0 release (#186)
    Changes:
    
    - Feature: Support create and update API endpoints
    - Feature: Support new personalProfile endpoint for creating JSON profiles based on user data
    - Feature: Include LLM response time in API responses
    - Feature: Adding support for directly using AWS bedrock SDK to access Llama 3.x using inference profiles
    - Feature: Support default provider and model in config
    - Feature: Support LLM token limit, flagging no system prompt available for LLM, including LLM info in API responses
    - Feature: Remove auto registration of new DIDs
    - Feature: Make RAG more configurable via API endpoint parameters
    - Fix: CORS was incorrectly configured
    - Fix: Improve handling of JSON responses, auto-cleanup when LLM returns invalid JSON
    - Fix: Escape HTML on client side to avoid JS code injection risks
    - Fix: Calendar events not indexed and available for personal prompt

--- src/api/rest/v1/llm/controller.ts
+++ src/api/rest/v1/llm/controller.ts
@@ -1,50 +1,64 @@
 import { Request, Response } from "express";
-import { LLMProviders, ProviderModels, prompt as LLMPrompt, OpenAIConfig, getLLM } from '../../../../services/llm'
+import { prompt as LLMPrompt, OpenAIConfig, getLLM } from '../../../../services/llm'
 import { PromptSearchService } from '../../../../services/assistants/search'
 import { Utils } from "../../../../utils";
 import { HotLoadProgress } from "../../../../services/data";
 import { DataService } from "../../../../services/data";
 import { PromptSearchServiceConfig } from "../../../../services/assistants/interfaces";
+import { PromptSearch } from "../../../../services/tools/promptSearch";
+import { LLMProvider, ProviderModels } from "../../../../services/llmmodels";
+import CONFIG from "../../../../config"
 const _ = require('lodash')
 
+const DEFAULT_LLM_MODEL = CONFIG.verida.llms.defaultModel
+const DEFAULT_LLM_PROVIDER = CONFIG.verida.llms.defaultProvider
+
 export interface LLMConfig {
-    llmProvider: LLMProviders,
+    llmProvider: LLMProvider,
     llmModel: string,
     customEndpoint?: OpenAIConfig
 }
 
-function buildLLMConfig(req: Request) {
-    const provider = req.body.provider ? req.body.provider.toString() : LLMProviders.BEDROCK.toString()
-    if (!Object.values(LLMProviders).includes(provider)) {
+function buildLLMConfig(req: Request): {
+    customEndpoint: OpenAIConfig
+    llmModelId: string
+    llmProvider: LLMProvider
+    llmTokenLimit?: number
+} {
+    const provider = req.body.provider ? req.body.provider.toString() : DEFAULT_LLM_PROVIDER
+    if (!Object.values(LLMProvider).includes(provider)) {
         throw new Error(`${provider} is not a valid LLM provider`)
     }
-    const llmProvider = <LLMProviders> provider
+    const llmProvider = <LLMProvider> provider
 
     let customEndpoint: OpenAIConfig
-    let llmModel: string
-    if (llmProvider == LLMProviders.CUSTOM) {
+    let llmModelId: string
+    if (llmProvider == LLMProvider.CUSTOM) {
         const endpoint = req.body.customEndpoint.toString()
         const key = req.body.customKey ? req.body.customKey.toString() : undefined
+        const noSystemPrompt = req.body.customNoSystemPrompt ? req.body.customNoSystemPrompt.toString() == "true" : false
         customEndpoint = {
             endpoint,
+            noSystemPrompt,
             key
         }
 
-        llmModel = req.body.model.toString()
+        llmModelId = req.body.model.toString()
     } else {
-        const model = req.body.model ? req.body.model.toString() : "LLAMA3_70B"
-        if (!Object.keys(ProviderModels[llmProvider]).includes(model)) {
-            throw new Error(`${model} is not a valid model for ${provider}`)
-        }
+        llmModelId = req.body.model ? req.body.model.toString() : DEFAULT_LLM_MODEL
 
-        // @ts-ignore
-        llmModel = ProviderModels[llmProvider][model]
+        if (!Object.keys(ProviderModels[llmProvider]).includes(llmModelId)) {
+            throw new Error(`${llmModelId} is not a valid model for ${provider}`)
+        }
     }
+
+    const llmTokenLimit = req.body.tokenLimit ? parseInt(req.body.tokenLimit.toString()) : undefined
 
     return {
         customEndpoint,
-        llmModel,
-        llmProvider
+        llmModelId,
+        llmProvider,
+        llmTokenLimit
     }
 }
 
@@ -57,20 +71,74 @@ export class LLMController {
         try {
             const {
                 customEndpoint,
-                llmModel,
-                llmProvider
+                llmModelId,
+                llmProvider,
+                llmTokenLimit
             } = buildLLMConfig(req)
 
             const prompt = req.body.prompt.toString()
             const systemPrompt = req.body.systemPrompt ? req.body.systemPrompt.toString() : undefined
             const jsonFormat = req.body.jsonFormat ? req.body.jsonFormat.toString() === "true" : false
-            const serverResponse = await LLMPrompt(prompt, systemPrompt, jsonFormat, llmProvider, llmModel, customEndpoint ? customEndpoint : undefined)
+            const serverResponse = await LLMPrompt(prompt, systemPrompt, jsonFormat, llmProvider, llmModelId, llmTokenLimit, customEndpoint ? customEndpoint : undefined)
 
             return res.json({
                 result: serverResponse
             })
         } catch (error) {
-            console.log(error)
+            console.error(error)
+            res.status(500).send({
+                success: false,
+                error: error.message
+            });
+        }
+    }
+
+    public async profilePrompt(req: Request, res: Response) {
+        try {
+            const { context, account } = await Utils.getNetworkConnectionFromRequest(req)
+            const did = await account.did()
+
+            const schema = req.body.schema
+            const promptSearchTip = req.body.promptSearchTip
+            const outputSystemPrompt = req.body.systemPrompt || false
+
+            const {
+                customEndpoint,
+                llmModelId,
+                llmProvider,
+                llmTokenLimit
+            } = buildLLMConfig(req)
+
+            const llm = getLLM(llmProvider, llmModelId, llmTokenLimit, customEndpoint)
+
+            let promptSearchResult = undefined
+            if (promptSearchTip) {
+                const promptSearch = new PromptSearch(llm)
+                promptSearchResult = await promptSearch.search(promptSearchTip)
+            }
+
+            const prompt = `Analyse my data to populate a JSON object that matches this schema.\n\n${schema}`
+            const promptConfig: PromptSearchServiceConfig = req.body.promptConfig ? req.body.promptConfig : {}
+            promptConfig.jsonFormat = true
+            promptConfig.promptSearchConfig = promptSearchResult
+
+            const promptService = new PromptSearchService(did, context)
+            const promptResult = await promptService.prompt(prompt, llm, promptConfig)
+
+            if (!outputSystemPrompt) {
+                promptResult.systemPrompt = undefined
+            }
+
+            promptResult.result = JSON.parse(promptResult.result)
+
+            promptResult.llm = {
+                provider: llmProvider,
+                model: llmModelId
+            }
+
+            return res.json(promptResult)
+        } catch (error) {
+            console.error(error)
             res.status(500).send({
                 success: false,
                 error: error.message
@@ -83,22 +151,31 @@ export class LLMController {
             const { context, account } = await Utils.getNetworkConnectionFromRequest(req)
             const did = await account.did()
             const prompt = req.body.prompt
-            const promptConfig: PromptSearchServiceConfig = req.body.promptConfig
+            let promptConfig: PromptSearchServiceConfig = req.body.promptConfig
+            promptConfig = _.merge({
+                jsonFormat: false
+            }, promptConfig ? promptConfig : {})
 
             const {
                 customEndpoint,
-                llmModel,
-                llmProvider
+                llmModelId,
+                llmProvider,
+                llmTokenLimit
             } = buildLLMConfig(req)
 
-            const llm = getLLM(llmProvider, llmModel, customEndpoint)
+            const llm = getLLM(llmProvider, llmModelId, llmTokenLimit, customEndpoint)
 
             const promptService = new PromptSearchService(did, context)
             const promptResult = await promptService.prompt(prompt, llm, promptConfig)
 
+            promptResult.llm = {
+                provider: llmProvider,
+                model: llmModelId
+            }
+
             return res.json(promptResult)
         } catch (error) {
-            console.log(error)
+            console.error(error)
             res.status(500).send({
                 success: false,
                 error: error.message
@@ -133,7 +210,7 @@ export class LLMController {
             await data.hotLoad()
             res.end()
         } catch (error) {
-            console.log(error)
+            console.error(error)
             res.write(`data: ${JSON.stringify({
                 success: false,
                 error: error.message
--- src/services/llm.ts
+++ src/services/llm.ts
@@ -1,39 +1,28 @@
 import Groq from "groq-sdk"
+import { BedrockRuntimeClient, ConverseCommand, ConverseCommandInput } from "@aws-sdk/client-bedrock-runtime"
 import Axios from 'axios'
 import CONFIG from "../config"
-
-export enum LLMProviders {
-  BEDROCK = "bedrock",
-  GROQ = "groq",
-  CUSTOM = "custom"
-}
-
-export const ProviderModels = {
-  [LLMProviders.BEDROCK]: {
-    "LLAMA3_70B": "meta.llama3-70b-instruct-v1:0",
-    "LLAMA3_8B": "meta.llama3-8b-instruct-v1:0",
-    "MIXTRAL_8_7B": "mistral.mixtral-8x7b-instruct-v0:1"
-  },
-  [LLMProviders.GROQ]: {
-    "LLAMA3_70B": "llama3-70b-8192",
-    "LLAMA3_8B": "llama3-8b-8192",
-    "LLAMA31_70B": "llama-3.1-70b-versatile",
-    "MIXTRAL8_7B": "mixtral-8x7b-32768"
-  },
-  [LLMProviders.CUSTOM]: {}
-}
+import { LLMProvider, ProviderModels } from "./llmmodels"
 
 export class LLMError extends Error {}
 
 const BEDROCK_KEY = CONFIG.verida.llms.bedrockKey
 const BEDROCK_ENDPOINT = CONFIG.verida.llms.bedrockEndpoint
+const BEDROCK_ACCESS_KEY_ID = CONFIG.verida.llms.bedrockAWSAccessKeyId
+const BEDROCK_SECRET_KEY = CONFIG.verida.llms.bedrockAWSSecretKey
 
-// const GROQ_MODEL = 'llama3-70b-8192' // 'llama3-8b-8192' // 'llama3-70b-8192' // 'llama3-8b-8192' //'llama-3.1-8b-instant' //'llama3-70b-8192' // 'llama3-8b-8192'
+const DEFAULT_LLM_MODEL = CONFIG.verida.llms.defaultModel
+const DEFAULT_LLM_PROVIDER = <LLMProvider> CONFIG.verida.llms.defaultProvider
+
 const GROQ_KEY = CONFIG.verida.llms.groqKey
 
 export interface OpenAIConfig {
+  // HTTP(s) endpoint for the Open AI server
   endpoint: string
+  // Bearer token key (optional)
   key?: string
+  // Indicate if the model doesn't support system prompts
+  noSystemPrompt?: boolean
 }
 
 export const LLMS: Record<string, OpenAIConfig> = {
@@ -48,36 +37,42 @@ export interface LLMResponse {
 }
 
 export interface LLM {
-  prompt(userPrompt: string, systemPrompt?: string, format?: boolean, model?: string): Promise<OpenAIChatResponse>
+  prompt(userPrompt: string, systemPrompt: string, jsonFormat?: boolean, model?: string): Promise<PromptResponse>
+  getContextTokens(model?: string): number
 }
 
-// export interface OpenAIChatResponse {
-//   id: string;
-//   object: string;
-//   created: number;
-//   model: string;
-//   choices: {
-//     index: number;
-//     message: {
-//       role: string;
-//       content: string;
-//     };
-//     finish_reason: string;
-//   }[];
-//   usage?: {
-//     prompt_tokens: number;
-//     completion_tokens: number;
-//     total_tokens: number;
-//   };
-// };
-export interface OpenAIChatResponse extends Groq.Chat.ChatCompletion {}
+export function stripNonJson(inputString: string) {
+  const startIndex = inputString.indexOf('{')
+  const endIndex = inputString.lastIndexOf('}')
+
+  if (startIndex === -1 || endIndex === -1 || startIndex > endIndex) {
+      return '' // Return an empty string if braces are not found or invalid
+  }
+
+  // Sometimes the LLM creates incorrect JSON by escaping "_"
+  return inputString.substring(startIndex, endIndex + 1).replace(/\\_/g,`_`)
+}
+
+export interface PromptUsage {
+  inputTokens: number
+  outputTokens: number
+  totalTokens: number
+}
+
+export interface PromptResponse {
+  textResponse: string
+  usage?: PromptUsage
+  timeMs?: number
+}
 
 export class GroqLLM implements LLM {
   private groq: Groq
   private defaultModel: string
+  private llmTokenLimit?: number
 
-  constructor(defaultModel: string) {
+  constructor(defaultModel: string, llmTokenLimit?: number) {
     this.defaultModel = defaultModel
+    this.llmTokenLimit = llmTokenLimit
 
     if (GROQ_KEY) {
       this.groq = new Groq({ apiKey: GROQ_KEY });
@@ -86,7 +81,20 @@ export class GroqLLM implements LLM {
     }
   }
 
-  public async prompt(userPrompt: string, systemPrompt?: string, jsonFormat: boolean = true, model: string = this.defaultModel): Promise<OpenAIChatResponse> {
+  public getContextTokens(modelId?: string): number {
+    if (this.llmTokenLimit) {
+      return this.llmTokenLimit
+    }
+
+    const model = ProviderModels[LLMProvider.GROQ][modelId || this.defaultModel]
+    return model.contextTokens
+  }
+
+  public async prompt(userPrompt: string, systemPrompt: string, jsonFormat: boolean = true, modelId: string = this.defaultModel): Promise<PromptResponse> {
+    if (jsonFormat) {
+      userPrompt += `\nOnly output a JSON object. Don't add any explanation or formatting.\n`
+    }
+
     const messages = [
       {
         role: "user",
@@ -101,36 +109,133 @@ export class GroqLLM implements LLM {
       })
     }
 
+    const model = ProviderModels[LLMProvider.GROQ][modelId]
     const response = await this.groq.chat.completions.create({
       response_format: jsonFormat ? {type: "json_object"} : undefined,
       // @ts-ignore
       messages,
-      model,
+      model: model.modelId,
       temperature: 1,
       top_p: 1
     });
 
-    return response
+    if (jsonFormat) {
+      response.choices[0].message.content = stripNonJson(response.choices[0].message.content!)
+    }
+
+    return {
+      textResponse: response.choices[0].message.content
+    }
+  }
 }
+
+export class BedrockLLM implements LLM {
+
+  private bedrock: BedrockRuntimeClient
+  private defaultModel: string
+  private llmTokenLimit?: number
+
+  constructor(defaultModel: string, llmTokenLimit?: number) {
+    this.defaultModel = defaultModel
+    this.llmTokenLimit = llmTokenLimit
+
+    this.bedrock = new BedrockRuntimeClient({ region: "us-east-1", credentials: {
+      accessKeyId: BEDROCK_ACCESS_KEY_ID,
+      secretAccessKey: BEDROCK_SECRET_KEY
+    }, });
+  }
+
+  public getContextTokens(modelId?: string): number {
+    if (this.llmTokenLimit) {
+      return this.llmTokenLimit
+    }
+
+    const model = ProviderModels[LLMProvider.BEDROCK][modelId || this.defaultModel]
+    return model.contextTokens
+  }
+  
+  public async prompt(userPrompt: string, systemPrompt: string, jsonFormat: boolean = true, modelId: string = this.defaultModel): Promise<PromptResponse> {
+    try {
+      const model = ProviderModels[LLMProvider.BEDROCK][modelId]
+
+      if (!model.systemPrompt) {
+        userPrompt = `${systemPrompt}\n\n${userPrompt}`
+      }
+
+      const input: ConverseCommandInput = {
+        modelId: model.modelId,
+        messages: [{
+          role: "user",
+          content: [{
+            text: userPrompt
+          }]
+        }]
+      }
+
+      if (systemPrompt && model.systemPrompt) {
+        input.system = [{
+          text: systemPrompt
+        }]
+      }
+
+      const command = new ConverseCommand(input)
+      const response = await this.bedrock.send(command)
+
+      if (response['$metadata'].httpStatusCode !== 200) {
+        console.error(response['$metadata'])
+        throw new Error(`Bedrock error`)
+      }
+
+      if (jsonFormat) {
+        response.output.message.content[0].text = stripNonJson(response.output.message.content[0].text)
+      }
+
+      // @ts-ignore
+      return {
+        textResponse: response.output.message.content[0].text
+      }
+
+    } catch (error: any) {
+      console.error(error)
+    }
+  }
 }
 
 export class OpenAILLM implements LLM {
 
   private config: OpenAIConfig
   private defaultModel: string
+  private llmTokenLimit?: number
 
-  constructor(config: OpenAIConfig, defaultModel: string) {
+  constructor(config: OpenAIConfig, defaultModel: string, llmTokenLimit?: number) {
     this.config = config
     this.defaultModel = defaultModel
+    this.llmTokenLimit = llmTokenLimit
   }
 
-  public async prompt(userPrompt: string, systemPrompt?: string, jsonFormat: boolean = true, model: string = this.defaultModel): Promise<OpenAIChatResponse> {
+  public getContextTokens(modelId?: string): number {
+    if (this.llmTokenLimit) {
+      return this.llmTokenLimit
+    }
+
+    throw new Error(`Unknown context token size for this model, please specify`)
+  }
+
+  public async prompt(userPrompt: string, systemPrompt: string, jsonFormat: boolean = true, modelId: string = this.defaultModel): Promise<PromptResponse> {
     const headers: Record<string, string> = {
       'Content-Type': 'application/json',
     }
 
     if (this.config.key) {
       headers['Authorization'] = `Bearer ${this.config.key}` 
+    }
+
+    if (jsonFormat) {
+      userPrompt += `\nOnly output a JSON object. Don't add any explanation or formatting.\n`
+    }
+
+    if (this.config.noSystemPrompt) {
+      userPrompt = `${systemPrompt}\n\n${userPrompt}`
     }
 
     const messages = [
@@ -140,7 +245,7 @@ export class OpenAILLM implements LLM {
       }
     ];
 
-    if (systemPrompt) {
+    if (systemPrompt && !this.config.noSystemPrompt) {
       messages.push({
         role: "system",
         content: systemPrompt,
@@ -151,15 +256,22 @@ export class OpenAILLM implements LLM {
       const response = await Axios.post(this.config.endpoint, {
         format: jsonFormat ? "json" : undefined,
         messages,
-        model,
+        model: modelId,
         temperature: 1,
         top_p: 1
       }, {
         headers
       });
 
-      return response.data
+      if (jsonFormat) {
+        response.data.choices[0].message.content = stripNonJson(response.data.choices[0].message.content!)
+      }
+
+      return {
+        textResponse: response.data.choices[0].message.content
+      }
     } catch (err: any) {
+      console.error(err)
       if (err.response?.data?.detail) {
         throw new LLMError(err.response.data.detail)
       }
@@ -169,27 +281,24 @@ export class OpenAILLM implements LLM {
   }
 }
 
-export function getLLM(provider: LLMProviders = LLMProviders.BEDROCK, model: string = "LLAMA3_70B", customEndpoint?: OpenAIConfig): LLM {
+export function getLLM(provider: LLMProvider = DEFAULT_LLM_PROVIDER, model: string = DEFAULT_LLM_MODEL, llmTokenLimit?: number, customEndpoint?: OpenAIConfig): LLM {
   let llm: LLM
   switch (provider) {
-    case LLMProviders.BEDROCK:
-      llm = new OpenAILLM(LLMS.BEDROCK, model)
+    case LLMProvider.BEDROCK:
+      // llm = new OpenAILLM(LLMS.BEDROCK, model)
+      llm = new BedrockLLM(model, llmTokenLimit)
       break
-    case LLMProviders.GROQ:
-      llm = new GroqLLM(model)
+    case LLMProvider.GROQ:
+      llm = new GroqLLM(model, llmTokenLimit)
       break
-    case LLMProviders.CUSTOM:
-      llm = new OpenAILLM(customEndpoint, model)
+    case LLMProvider.CUSTOM:
+      llm = new OpenAILLM(customEndpoint, model, llmTokenLimit)
   }
 
   return llm
 }
 
-export async function prompt(userPrompt: string, systemPrompt?: string, jsonFormat: boolean = true, provider: LLMProviders = LLMProviders.BEDROCK, model: string = "LLAMA3_70B", customEndpoint?: OpenAIConfig): Promise<OpenAIChatResponse> {
-  const llm = getLLM(provider, model, customEndpoint)
+export async function prompt(userPrompt: string, systemPrompt: string, jsonFormat: boolean = true, provider: LLMProvider = DEFAULT_LLM_PROVIDER, model: string = DEFAULT_LLM_MODEL, llmTokenLimit?: number, customEndpoint?: OpenAIConfig): Promise<PromptResponse> {
+  const llm = getLLM(provider, model, llmTokenLimit, customEndpoint)
   return llm.prompt(userPrompt, systemPrompt, jsonFormat, model)
 }
-
-export const bedrock = new OpenAILLM(LLMS.BEDROCK, "LLAMA3_70B")
-export const groq = new GroqLLM("LLAMA3_70B")
-export const defaultModel = bedrock
