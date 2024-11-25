e177116fdcd09d697d1c91fede1b23b4ef7f0467
Chris <chris@verida.io>
2024-11-25 14:56:19 +1030

    Fix merge issue with server.ts running via HTTPS when it shouldn't be.

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,7 @@
-/*
-This lets us run the Express server locally 
-*/
 const app = require('./server-app');
-
 
 const PORT = process.env.SERVER_PORT ? process.env.SERVER_PORT : 5021;
 
-
-// app.listen(PORT, () => {
-//   console.log(`server running on port ${PORT}`);
-// });
-
-const https = require("https")
-const fs = require("fs")
-
-const key = fs.readFileSync("./keys/server.key")
-const cert = fs.readFileSync("./keys/server.cert")
-
-https.createServer(
-    {
-      key,
-      cert
-    },
-    app
-  ).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
