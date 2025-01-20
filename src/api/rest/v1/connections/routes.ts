5ba028552be5b548ccf10fc1e5506b74b6358310
Chris <chris@verida.io>
2025-01-21 09:50:46 +1030

    Require session auth for connection routes

--- src/api/rest/v1/connections/routes.ts
+++ src/api/rest/v1/connections/routes.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import Controller from './controller'
+import auth from "../../../../middleware/auth";
 
 const router = express.Router()
+const sessionAuth = auth({
+    sessionRequired: true
+})
 
-router.get('/', Controller.connections)
-router.post('/sync', Controller.sync)
-router.post('/:connectionId/sync', Controller.syncConnection)
-router.put('/:connectionId', Controller.update)
-router.delete('/:connectionId', Controller.disconnect)
+router.get('/', sessionAuth, Controller.connections)
+router.post('/sync', sessionAuth, auth(), Controller.sync)
+router.post('/:connectionId/sync', sessionAuth, auth(), Controller.syncConnection)
+router.put('/:connectionId', sessionAuth, auth(), Controller.update)
+router.delete('/:connectionId', sessionAuth, auth(), Controller.disconnect)
 
 export default router
