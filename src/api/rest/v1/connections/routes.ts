e1a379458ca375a8f114451d8e2b367895091e58
Chris <chris@verida.io>
2025-01-22 10:25:45 +1030

    Fix incorrect auth config on connection routes

--- src/api/rest/v1/connections/routes.ts
+++ src/api/rest/v1/connections/routes.ts
@@ -3,14 +3,11 @@ import Controller from './controller'
 import auth from "../../../../middleware/auth";
 
 const router = express.Router()
-const sessionAuth = auth({
-    sessionRequired: true
-})
 
-router.get('/', sessionAuth, Controller.connections)
-router.post('/sync', sessionAuth, auth(), Controller.sync)
-router.post('/:connectionId/sync', sessionAuth, auth(), Controller.syncConnection)
-router.put('/:connectionId', sessionAuth, auth(), Controller.update)
-router.delete('/:connectionId', sessionAuth, auth(), Controller.disconnect)
+router.get('/', auth(), Controller.connections)
+router.post('/sync', auth(), Controller.sync)
+router.post('/:connectionId/sync', auth(), Controller.syncConnection)
+router.put('/:connectionId', auth(), Controller.update)
+router.delete('/:connectionId', auth(), Controller.disconnect)
 
 export default router
