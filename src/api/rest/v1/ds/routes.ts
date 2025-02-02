d18fbc55ad8e810f809ffebbff9a7c793d8d5cba
Chris <chris@verida.io>
2025-02-02 18:04:39 +1030

    Fix datastore routes

--- src/api/rest/v1/ds/routes.ts
+++ src/api/rest/v1/ds/routes.ts
@@ -5,45 +5,45 @@ import CONFIG from "../../../../config"
 
 const router = express.Router()
 
-router.get("/get/:schema/:recordId",  auth({
+router.get("/get/:schema/:recordId", auth({
     scopes: ["api:ds-get-by-id"],
-    dbScope: "r",
+    dsScope: "r",
     credits: CONFIG.verida.billing.defaultCredits
 }), controller.getById)
 
 router.post("/query/:schema", auth({
     scopes: ["api:ds-query"],
-    dbScope: "r",
+    dsScope: "r",
     credits: CONFIG.verida.billing.defaultCredits
 }),  controller.query)
 
 router.delete("/:schema", auth({
     scopes: ["api:ds-delete"],
-    dbScope: "d",
+    dsScope: "d",
     credits: CONFIG.verida.billing.defaultCredits
 }),  controller.delete)
 
 router.delete("/:schema/:recordId", auth({
     scopes: ["api:ds-delete"],
-    dbScope: "d",
+    dsScope: "d",
     credits: CONFIG.verida.billing.defaultCredits
 }),  controller.delete)
 
 router.get("/watch/:schema", auth({
     scopes: ["api:ds-query"],
-    dbScope: "r",
+    dsScope: "r",
     credits: CONFIG.verida.billing.defaultCredits
 }),  controller.watch)
 
 router.post("/:schema", auth({
     scopes: ["api:ds-create"],
-    dbScope: "w",
+    dsScope: "w",
     credits: CONFIG.verida.billing.defaultCredits
 }),  controller.create)
 
 router.put("/:schema/:recordId", auth({
     scopes: ["api:ds-update"],
-    dbScope: "w",
+    dsScope: "w",
     credits: CONFIG.verida.billing.defaultCredits
 }), controller.update)
 
