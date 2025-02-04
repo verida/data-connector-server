90ea3a7c0e43f85f3d7a782fd9f14e053f27549b
Chris <chris@verida.io>
2025-02-04 10:32:28 +1030

    Ensure server runs on HTTP on main branch

--- src/server.ts
+++ src/server.ts
@@ -6,24 +6,24 @@ import BillingManager from "./services/billing/manager"
 const PORT = process.env.SERVER_PORT ? process.env.SERVER_PORT : 5021;
 
 
-// app.listen(PORT, () => {
-//   console.log(`server running on port ${PORT}`);
-// });
-
-const https = require("https")
-const fs = require("fs")
-
-const key = fs.readFileSync("./keys/server.key")
-const cert = fs.readFileSync("./keys/server.cert")
-
-https.createServer(
-    {
-      key,
-      cert
-    },
-    app
-  ).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-  UsageManager.buildIndexes()
-  BillingManager.buildIndexes()
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
+
+// const https = require("https")
+// const fs = require("fs")
+
+// const key = fs.readFileSync("./keys/server.key")
+// const cert = fs.readFileSync("./keys/server.cert")
+
+// https.createServer(
+//     {
+//       key,
+//       cert
+//     },
+//     app
+//   ).listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+//   UsageManager.buildIndexes()
+//   BillingManager.buildIndexes()
+// });
