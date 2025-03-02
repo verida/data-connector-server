796ec34016a59c04ea950330a27a0ccbe0c8f0eb
aurelticot <aurelticot.dev@gmail.com>
2025-03-03 10:07:18 +1100

    chore: target `develop` merging

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,27 @@ import UsageManager from "./services/usage/manager"
 import BillingManager from "./services/billing/manager"
 import { BackgroundSyncManager } from "./services/backgroundSync";
 
+
 const PORT = process.env.SERVER_PORT ? process.env.SERVER_PORT : 5021;
 
-app.listen(PORT, () => {
+
+// app.listen(PORT, () => {
+//   console.log(`server running on port ${PORT}`);
+// });
+
+const https = require("https")
+const fs = require("fs")
+
+const key = fs.readFileSync("./keys/server.key")
+const cert = fs.readFileSync("./keys/server.cert")
+
+https.createServer(
+    {
+      key,
+      cert
+    },
+    app
+  ).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   UsageManager.buildIndexes()
   BillingManager.buildIndexes()
