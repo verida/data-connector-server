550cfbe02ed94cf5c6783b9ea4c00044f639dc53
Chris <chris@verida.io>
2025-03-03 18:19:18 +1030

    Support updating config via API endpoint

--- src/api/rest/v1/admin/routes.ts
+++ src/api/rest/v1/admin/routes.ts
@@ -1,6 +1,11 @@
 import express from 'express'
 import { controller } from './controller'
 import auth from "../../../../middleware/auth";
+import multer from 'multer';
+
+// Configure Multer to store uploaded files in memory
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 const router = express.Router()
 
@@ -11,5 +16,9 @@ router.get(/logs$/, auth({
 router.get(/clearLogs$/, auth({
     options: { checkAdmin: true }
 }), controller.clearLogs)
+
+router.post(/updateConfig$/, upload.single('file'), auth({
+    options: { checkAdmin: true }
+}), controller.updateConfig)
 
 export default router
