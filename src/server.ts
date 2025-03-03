531af24f0d093ddafdba757f43781d39e0270636
Chris <chris@verida.io>
2025-03-03 16:37:30 +1030

    Also listen for SIGINT

--- src/server.ts
+++ src/server.ts
@@ -42,3 +42,16 @@ process.on('SIGTERM', () => {
     process.exit(1);
   }, 10000);
 });
+
+process.on('SIGINT', () => {
+  console.log('Received SIGINT, shutting down gracefully...');
+  server.close(() => {
+    console.log('Closed out remaining connections.');
+    process.exit(0);
+  });
+  // Force shutdown if not closed in 10 seconds
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+});
