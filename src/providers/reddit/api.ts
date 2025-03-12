e6cd535f849ca71600d585e8200473097d3e38f9
Peter <kompaszbence@gmail.com>
2025-03-12 12:21:25 +0200

    feat: api fetch subreddits, profile

--- src/providers/reddit/api.ts
+++ src/providers/reddit/api.ts
@@ -1,10 +1,13 @@
-import { Devvit } from "@devvit/public-api";
-import axios, { Axios, AxiosInterceptorOptions } from "axios";
+import { Devvit, Subreddit } from "@devvit/public-api";
+import axios, { Axios, AxiosInterceptorOptions, AxiosResponse } from "axios";
 
-const requestInterceptor = [
-  function (config: any) {
+const OAUTH_ENDPOINT = "https://oauth.reddit.com/";
+
+const requestInterceptor = (token: string) => [
+  function (request: any) {
+    request.headers.Authorization = `Bearer ${token}`;
     // Do something before request is sent
-    return config;
+    return request;
   },
   function (error: any) {
     // Do something with request error
@@ -20,7 +23,7 @@ const requestInterceptor = [
  *  X-Ratelimit-Reset: Approximate number of seconds to end of period
  */
 const responseInterceptor = [
-  function (response: any) {
+  function (response: AxiosResponse) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
@@ -32,9 +35,16 @@ const responseInterceptor = [
   },
 ];
 
-type RequestConfig = {};
+type RequestConfig = {
+  max_replies?: number;
+  pagination?: PaginationParams;
+};
 
 type Chat = {};
+
+type PaginationParams = {
+  after: string;
+};
 
 /**
  * @abstract
@@ -43,23 +53,14 @@ type Chat = {};
 export class RedditApi {
   clientId: string;
   tdPath: string;
-  client?: Axios | Devvit;
+  client?: Axios;
 
-  constructor(clientId: string, binFile?: string) {
+  constructor(clientId: string) {
     this.clientId = clientId;
-
-    // TODO Research this, might be used to store auth token
-    // this.tdPath = `${tdPathPrefix}/${this.clientId}`;
-    // if (binFile) {
-    //   this.restoreBinFile(binFile);
-    // }
   }
 
   // Devvit is under development
-  public async getClient(
-    useDevvit: boolean = false,
-    startClient: boolean = false
-  ): Promise<Devvit | Axios> {
+  public async getClient(useDevvit: boolean = false): Promise<Devvit | Axios> {
     if (this.client) {
       return this.client;
     }
@@ -70,14 +71,12 @@ export class RedditApi {
       } else {
         this.client = axios.create();
 
-        this.client.interceptors.request.use(...requestInterceptor);
+        this.client.interceptors.request.use(
+          ...requestInterceptor(this.clientId)
+        );
 
         // Add a response interceptor
         this.client.interceptors.response.use(...responseInterceptor);
-      }
-
-      if (startClient) {
-        await this.startClient();
       }
 
       return this.client;
@@ -87,33 +86,67 @@ export class RedditApi {
     }
   }
 
-  public async startClient() {
-    const client = await this.getClient();
-
-    // load saved binary file to disk
-    // const nowMinutes = Math.floor(Date.now() / 1000 / 60)
-  }
-
+  /**
+   *
+   * @summary This functions makes a request to the reddit API endpoint. It assumes that requested data is either a single entity or a listing
+   * In case of a listing a custom interceptor has to set the terminationCriteriaMet flag to true and the pagination params.
+   * The returned value is always a Type array, where Type is the supplied type.
+   * @param endpoint
+   * @param config
+   * @param customInterceptor
+   * @returns The returned data is either an array or an array of arrays. We can extract the data in the caller function based on the requested type.
+   * @example profile -> resp[0] messages -> resp.flat() or subreddits -> resp.flat().flat() (multiple types of subreddits)
+   */
   private async _call<Type>(
-    endpoint: string,
+    method: "GET" | "POST" | "PUT",
+    endpoint: `${string}.json`,
     config?: RequestConfig,
     customInterceptor?: any[]
-  ): Promise<Type | Type[]> {
-    return {} as Type;
-  }
+  ): Promise<Type[] | undefined> {
+    let terminationCriteriaMet = true;
+    let data: Type[] = [];
+    let pagination: PaginationParams = config.pagination;
 
-  public async getGroupChat(chatId: number): Promise<Chat> {
-    const chatDetail = await this._call("", {
-      chat_id: chatId,
-    });
+    const myInterceptor = axios.interceptors.request.use(...customInterceptor);
 
-    return chatDetail;
+    let action;
+    switch (method) {
+      case "GET":
+        action = this.client.get;
+        break;
+      case "POST":
+        action = this.client.post;
+        break;
+      case "PUT":
+        action = this.client.put;
+        break;
+    }
+
+    while (terminationCriteriaMet) {
+      const resp = await action<{
+        data: Type;
+        terminationCriteriaMet: boolean;
+        pagination: PaginationParams;
+      }>(`${OAUTH_ENDPOINT}${endpoint}?after=${after}`, {
+        params: {
+          ...config,
+          ...pagination,
+        },
+      });
+      data.push(resp.data.data);
+      terminationCriteriaMet = resp.data.terminationCriteriaMet;
+      pagination = resp.data.pagination;
+    }
+
+    this.client.interceptors.request.eject(myInterceptor);
+
+    return data;
   }
 
   /**
    *
    * @summary Read all private messages from inbox, unread and sent folder from the past 3 months
-   * https://www.reddit.com/dev/api#GET_message_{where}
+   * @see https://www.reddit.com/dev/api#GET_message_{where}
    * @returns
    */
   public async getChats(): Promise<Chat[]> {
@@ -121,7 +154,8 @@ export class RedditApi {
     const customInterceptor: any[] = [];
 
     const chatDetail = await this._call<Chat[]>(
-      "/message/inbox",
+      "GET",
+      "/message/inbox.json",
       {
         max_replies: 300,
       },
@@ -136,20 +170,47 @@ export class RedditApi {
    * @summary Get the profile of auth token owner
    * @returns
    */
-  public async getMe(): Promise<any> {
-    const client = await this.getClient();
-    const user = await this._call("/api/v1/me");
+  public async getMe(): Promise<string> {
+    const user = await this._call<string>("GET", "/api/v1/me.json");
 
-    return user;
+    return user[0];
   }
 
   /**
    *
    * @summary Get subreddits where the user is subcribed to, a contributor or a moderator
+   * @see https://www.reddit.com/dev/api#GET_subreddits_mine_{where}
    * @returns
    */
-  public async getSubreddits() {
-    const client = await this.getClient();
-    return await this._call("/subreddits/mine/subscribe", {});
+  public async getSubreddits(): Promise<Subreddit[] | undefined> {
+    const endpoints: `${string}.json`[] = [
+      "/subreddits/mine/contributor.json",
+      "/subreddits/mine/moderator.json",
+      "/subreddits/mine/subscribe.json",
+    ];
+    const subreddits = await Promise.all(
+      endpoints.map(async (endpoint) => {
+        return await this._call<Subreddit[]>(
+          "GET",
+          "/message/inbox.json",
+          {
+            max_replies: 300,
+          },
+          [
+            (response: AxiosResponse) => {
+              if (response.data.children.length === 0) {
+                return {
+                  data: [] as any[],
+                  terminationCriteriaMet: true,
+                  pagination: {},
+                };
+              }
+            },
+          ]
+        );
+      })
+    );
+
+    return subreddits.flat().flat();
   }
 }
