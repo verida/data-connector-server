072dc67f253240a19cb17f286841bec7c698a485
Peter <kompaszbence@gmail.com>
2025-05-06 22:53:16 +0300

    feat: api tests

--- tests/providers/reddit/api.test.ts
+++ tests/providers/reddit/api.test.ts
@@ -1,46 +1,83 @@
-import { Listing, PrivateMessage, User, Subreddit } from "@devvit/public-api";
+// import { Listing, PrivateMessage, User, Subreddit } from "@devvit/public-api";
+import { Connection } from "../../../src/interfaces";
 import { RedditApi } from "../../../src/providers/reddit/api";
-import { z } from "zod";
+import { CommonTests } from "../../common.tests";
+import CommonUtils from "../../common.utils";
+const assert = require("assert");
+import ChatsHandler from "../../../src/providers/reddit/chat";
+import { before } from "mocha/lib/mocha";
+import { API_CONFIG } from "./config";
 
-// TODO Read from .env file
-const clientId = "";
-const reddit = new RedditApi(clientId);
-const API_CALLS = [
-  {
-    endpoint: "chats",
-    action: reddit.getChats,
-    params: ["inbox", 100],
-    expectedType: Listing<PrivateMessage>,
-  },
-  {
-    endpoint: "me",
-    action: reddit.getMe,
-    expectedType: User,
-  },
-  {
-    endpoint: "user",
-    action: reddit.getUser,
-    params: [""],
-    expectedType: User,
-  },
-  {
-    endpoint: "subreddits",
-    action: reddit.getSubreddits,
-    expectedType: Listing<Subreddit>,
-  },
-];
+let connection: Connection;
+let api;
 
-/**
- * @summary Test that API calls returns a result w/ the expected type
- */
+const d = new Date();
+const twoMonthsAgo = d.setMonth(d.getMonth() - 2);
+
 describe("Reddit API test", async () => {
-  it("should fetch valid data for each supported endpoint", async () => {
-    await Promise.all(
-      API_CALLS.map(async (apiCall) => {
-        const { params, action } = apiCall;
-        // @ts-expect-error Just for testing
-        const resp = params ? action(...params) : action;
-      })
+  before(async () => {
+    connection = await CommonUtils.getConnection("reddit");
+
+    const objects = await CommonTests.buildTestObjects(
+      "reddit",
+      ChatsHandler,
+      {},
+      connection
     );
+    api = objects.api;
+  });
+
+  it("should fetch me", async () => {
+    const me = await api.getMe();
+    assert.notStrictEqual(me, null);
+  });
+
+  it.skip("should fetch user", async () => {
+    // TODO Fetch from config
+    const user = await api.getUser(API_CONFIG.username);
+    assert.notStrictEqual(user, null);
+  });
+
+  it("should fetch privates messages", async () => {
+    // Get all the messages
+    const allMessages = await api.getMessages();
+    assert(allMessages > 0, "No messages fetched");
+
+    const unreadMessages = await api.getMessages("unread");
+    assert(
+      allMessages > unreadMessages,
+      "All messages should contain unreadMessages"
+    );
+  });
+
+  it("should fetch privates messages", async () => {
+    // Get all the messages
+    const privateMessages = await api.getMessages("private");
+    assert(privateMessages > 0, "No messages fetched");
+
+    // Fetch messages in the last 2 months
+    const privateMessagesLast2Months = await api.getMessages("private", {
+      untilDate: twoMonthsAgo,
+    });
+
+    // There should be more messages in the last 3 months than last 2 months
+    assert(
+      privateMessages <= privateMessagesLast2Months,
+      "No messages fetched"
+    );
+  });
+
+  it("should fetch comments", async () => {
+    const comments = await api.getComments();
+
+    assert(comments.length > 0, "No comments found");
+  });
+
+  it("should fetch subreddits", async () => {
+    const allSubreddits = await api.getSubreddits();
+    assert(allSubreddits.length > 0, "No subreddits found");
+
+    const moderatedSubreddits = await api.getSubreddits("moderator");
+    assert(moderatedSubreddits.length > 0, "No moderated subreddits found");
   });
 });
